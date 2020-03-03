5781977225f58ccfd73e30cad8189cf88bb4eab7
jczuchnowski <jakub.czuchnowski@gmail.com>
2020-03-04 00:31:10 +0100

    create ZookeeperClient service

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,70 @@ struct ZNode {
     mode: Mode,
 }
 
+struct ZookeeperClient {
+    host: String,
+    port: String,
+}
+
+impl ZookeeperClient {
+
+    fn new(host: String, port: String) -> ZookeeperClient {
+        ZookeeperClient {
+            host: host,
+            port: port,
+        }
+    }
+
+    fn call_nc(&self, command: &String, grep: &String) -> Option<String> {
+        let com = format!("echo -n '{}' | nc -w 5 {} {} | grep {}", command, self.host, self.port, grep);
+    
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg(com)
+            .output()
+            .expect("no connection");
+    
+        let output_status = output.status;
+    
+        if output_status.success() {
+            let mut output_std: Vec<u8> = output.stdout.clone();
+            output_std.truncate(output_std.len() - 1);
+            let pref_len = grep.len();
+            let output_std_f: Vec<u8> = output_std.drain(pref_len+1..).collect();
+            let output_str = str::from_utf8(&output_std_f).unwrap();
+            
+            return Some(output_str.trim().to_string());
+        } else {
+            return None;
+        }
+    }
+
+    fn get_status(&self) -> Option<ZNode> {
+        let mode = self.call_nc(&"srvr".to_string(), &"Mode".to_string());
+        let server_id = self.call_nc(&"conf".to_string(), &"serverId".to_string());
+
+        let znode: Option<ZNode> = match (mode, server_id) {
+            (Some(m), Some(id)) => {
+                let mode = match m.as_str() {
+                    "leader"   => Mode::Leader,
+                    "follower" => Mode::Follower,
+                    _ => Mode::Unknown,
+                };
+
+                let znode = ZNode {
+                    id: id,
+                    mode: mode
+                };
+
+                Some(znode)
+            },
+            _ => None
+        };
+
+        znode
+    }
+}
+
 /// Splits a String of format `host:port` into a tuple.
 fn split(s: &String) -> (String, String) {
     let pair_vec: Vec<String> = s.split(':').map(|s| s.to_string()).collect();
@@ -52,31 +116,6 @@ fn max_len(v: &Vec<&String>) -> usize {
     });
 
     return max_host.len();
-}
-
-//TODO refactor into a service
-fn call_zookeeper(host: &String, port: &String, command: &String, grep: &String) -> Option<String> {
-    let com = format!("echo -n '{}' | nc -w 5 {} {} | grep {}", command, host, port, grep);
-
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg(com)
-        .output()
-        .expect("no connection");
-
-    let output_status = output.status;
-
-    if output_status.success() {
-        let mut output_std: Vec<u8> = output.stdout.clone();
-        output_std.truncate(output_std.len() - 1);
-        let pref_len = grep.len();
-        let output_std_f: Vec<u8> = output_std.drain(pref_len+1..).collect();
-        let output_str = str::from_utf8(&output_std_f).unwrap();
-        
-        return Some(output_str.trim().to_string());
-    } else {
-        return None;
-    }
 }
 
 fn main() {
@@ -104,30 +143,9 @@ fn main() {
         let p = port.clone();
 
         threads.push(thread::spawn(move || {
-
-            let mode = call_zookeeper(&h, &p, &"srvr".to_string(), &"Mode".to_string());
-            let server_id = call_zookeeper(&h, &p, &"conf".to_string(), &"serverId".to_string());
-
-            let znode: Option<ZNode> = match (mode, server_id) {
-                (Some(m), Some(id)) => {
-                    let mode = match m.as_str() {
-                        "leader"   => Mode::Leader,
-                        "follower" => Mode::Follower,
-                        _ => Mode::Unknown,
-                    };
-
-                    let znode = ZNode {
-                        id: id,
-                        mode: mode
-                    };
-
-                    Some(znode)
-                },
-                _ => None
-            };
-
+            let client = ZookeeperClient::new(h.clone(), p);
+            let znode = client.get_status();
             txc.send((h, znode));
-
         }));
 
     }
